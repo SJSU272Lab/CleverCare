083374e72489e74be84f8d1b0a5b0431b868e571
siddharth.daftari <siddharthrajesh.daftari@sjsu.edu>
2016-12-09 11:04:41 -0800

    dashboard chart changes

--- project/clevercare-client/public/controller/dashboardController.js
+++ project/clevercare-client/public/controller/dashboardController.js
@@ -1,63 +1,14 @@
-rhrApp.controller('dashboardController', function dashboardController($scope,$http,$filter) {
+rhrApp.controller('dashboardController', function dashboardController($scope) {
   
   //to be removed in code cleanup
   console.log("dashboardController : going in");
-
-    /*var d = {email_id: email_id, password: pwd};
-    $http.post('/signin', d)
-        .success(function (data) {
-
-        })
-        .error(function (data) {
-
-        });
-*/
-
-    $http.get("/listFollowUps/Total")
-        .success(function (response) {
-            $scope.dashboardData.patientCount = Number(response.notDone);
-            $scope.dashboardData.nurseProgressChartData = [
-                {label: 'Done', value: response.done },
-                {label: 'Not Done', value: response.notDone }];
-        })
-        .error(function(data) {
-
-        });
-
-    $http.get("/listFollowUps/Critical")
-        .success(function (response) {
-            var criticalArr = [];
-            for(var i=0;i<response.length;i++){
-                console.log(response[i]);
-                    var criticalObj = {
-                        id:i+1,
-                        name:response[i].patientId.firstname+" "+response[i].patientId.lastname,
-                        status:response[i].status,
-                        lastVisit: $filter('date')(response[i].dueDate, "MM/dd/yyyy")
-                    }
-               criticalArr.push(criticalObj);
-            }
-            $scope.dashboardData.recentFivePatient = criticalArr;
-            $scope.dashboardData.recentPatientCount = response.length;
-        })
-        .error(function(data) {
-
-        });
-
-    $http.get("/notes/count/583f4b898a432621ccf06265?usertype=doctor")
-        .success(function (response) {
-            $scope.dashboardData.noteCount = response.notes.length;
-        })
-        .error(function(data) {
-
-        });
 
   $scope.userdata = {};
   $scope.dashboardData = {};
   $scope.userdata.usertype = 'nurse';
 
   //dummy data 
- // $scope.dashboardData.patientCount = 40;
+  $scope.dashboardData.patientCount = 40;
   $scope.dashboardData.noteCount = 30;
   $scope.dashboardData.testCount = 111;
 
@@ -68,12 +19,21 @@ rhrApp.controller('dashboardController', function dashboardController($scope,$ht
             {label: 'Progress', value: (100 - $scope.dashboardData.nurseProgress) }];
 
 
- /* $scope.dashboardData.recentFivePatient = [{ 'id' : 1, 'name' : 'mathew wayne' , 'address' : '101 S 4th ST, san jose, CA' , 'status' : 'visit required' , 'lastVisit' : '11/06/9016'},
+  $scope.dashboardData.recentFivePatient = [{ 'id' : 1, 'name' : 'mathew wayne' , 'address' : '101 S 4th ST, san jose, CA' , 'status' : 'visit required' , 'lastVisit' : '11/06/9016'},
               { 'id' : 7, 'name' : 'allen smith' , 'address' : '33 S 3th ST, san jose, CA' , 'status' : 'feedback required' , 'lastVisit' : '11/0/9016'},
               { 'id' : 3, 'name' : 'anderson cooper' , 'address' : '901 S 4th ST, san jose, CA' , 'status' : 'feddback required' , 'lastVisit' : '10/01/9016'},
               { 'id' : 4, 'name' : 'barry watson' , 'address' : '101 S 4th ST, san jose, CA' , 'status' : 'visit required' , 'lastVisit' : '09/03/9016'},
               { 'id' : 5, 'name' : 'bruce perry' , 'address' : '901 S 4th ST, san jose, CA' , 'status' : 'visit required' , 'lastVisit' : '09/06/9016'}];
-*/
+  
+  
+
+  $scope.dashboardData.productTypeData = [{"weekday": "Monday", "cases": 4 },
+                                          {"weekday": "Tuesday", "cases": 8 },
+                                          {"weekday": "Wednesday", "cases": 31},
+                                          {"weekday": "Thursday", "cases": 5},
+                                          {"weekday": "Friday", "cases": 61 },
+                                          {"weekday": "Saturday", "cases": 6 },
+                                          {"weekday": "Sunday", "cases": 7}];
   //to be removed in code cleanup
   console.log("dashboardController : moving out");
 
